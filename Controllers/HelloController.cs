4728d14fa8a214c89fbf44ec774ebf96be0dc51b
dValsc <valeria.dcarvalho@hotmail.com>
2023-09-21 17:25:37 -0300

    hello world

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Valéria";
+        }
+
+    }
+}
+
+
